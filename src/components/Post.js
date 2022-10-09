413437bc9982b6b71fc1cded9f55c9aaaaaf993e
kush002 <kushagrasingh543@gmail.com>
2022-10-09 23:58:41 +0530

    Add files via upload

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,9 @@
 import React from "react";
-const Post = (props) => {
-  const dataTitle = props.details.map((title) => {
-    return (
-      <div className="post" key={title.id}>
-        <h3>{title.title}</h3>
-        <p>{title.body}</p>
-      </div>
-    );
-  });
+const Post = ({ ele }) => {
   return (
     <div className="post">
-      {dataTitle}
-      <p></p>
+      <h3>{ele.title}</h3>
+      <p>{ele.body}</p>
     </div>
   );
 };
