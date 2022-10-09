331b6d9e2312e764d8b21abb834b585fcfd7a89d
kush002 <kushagrasingh543@gmail.com>
2022-10-09 22:09:05 +0530

    Add files via upload

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,19 @@
-import React from 'react'
-const Post = () => {
+import React from "react";
+const Post = (props) => {
+  const dataTitle = props.details.map((title) => {
     return (
-        <div className="post">
+      <div className="post" key={title.id}>
+        <h3>{title.title}</h3>
+        <p>{title.body}</p>
+      </div>
+    );
+  });
+  return (
+    <div className="post">
+      {dataTitle}
+      <p></p>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export { Post }
+export { Post };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,41 @@
-import React from 'react'
-
-
+import React, { useEffect, useState } from "react";
+import { Post } from "./Post";
+import { PaginationButtonsList } from "./PaginationButtonsList";
 const PostList = () => {
-    
-    return (
-        <>
+  const [data, setData] = useState([]);
+  const [pageId, setPageId] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const fetchingData = async () => {
+    // setLoading(false);
 
-        </>
-    )
-}
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?_page=${pageId}&_limit=5`
+      );
+      const data = await response.json();
+      setData(data);
 
-export { PostList }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    setLoading(false);
+    fetchingData();
+    console.log(loading);
+  }, [pageId]);
+  const pageNumberHandler = (pageNumber) => {
+    setPageId(pageNumber);
+    setLoading(true);
+  };
+  //   console.log(loading);
+  return (
+    <>
+      {loading ? <Post details={data} /> : <h1 id="loader">loading...</h1>}
+      <PaginationButtonsList details={data} onClick={pageNumberHandler} />
+    </>
+  );
+};
+
+export { PostList };
