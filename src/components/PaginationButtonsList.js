9fd0b73f174404315e4efe216ad56860213387e7
kush002 <kushagrasingh543@gmail.com>
2022-10-14 23:32:34 +0530

    Update PaginationButtonsList.js

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
+const PaginationButtonsList = ({ page, clickHandler }) => {
+  let pages = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
 
-const buttonArray = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
-
-const PaginationButtonsList = (props) => {
-  const [pageNum, setPageNum] = useState(1);
-  props.onClick(pageNum);
-  const button = buttonArray.map((btn) => (
-    <button
-      className={btn == pageNum ? "active-btn" : ""}
-      key={`${btn}`}
-      onClick={() => {
-        setPageNum(`${btn}`);
-      }}
-      id={`button-${btn}`}
-    >{`${btn}`}</button>
-  ));
-  return <div className="pagination-buttons-list">{button}</div>;
+  return (
+    <div className="pagination-buttons-list">
+      {pages.map((ele, id) => {
+        return (
+          <button
+            className={ele == page ? "active-btn" : ""}
+            id={`button-${ele}`}
+            key={id}
+            onClick={() => {
+              clickHandler(ele);
+            }}
+          >
+            {ele}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export { PaginationButtonsList };
